4790b0c12203524c82c5bb6ea681043365d740e1
Jimmy <jimmyvoskuil@msn.com>
2016-12-13 21:46:18 +0100

    see the weather all over the place

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?appid=9d062079907494e9633226d4bbb75f00&units=metric"
+
+module.exports = {
+	getTemp: function (location) {
+		var encodedLocation = encodeURIComponent(location)
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+		return axios.get(requestUrl).then( function (res) {
+			if (res.data.cod && res.data.message) {
+				throw new Error(res.data.message)
+			} else {
+				return res.data.main.temp
+			}
+		}, function (res) {
+			throw new Error(res.data.message)
+		})
+	}
+}
